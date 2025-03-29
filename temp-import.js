8023395c80f8b8d3db20182461baae9c7feae2f2
chrisweb <chris963@gmail.com>
2025-03-29 20:43:35 +0100

    cleanup

--- temp-import.js
+++
@@ -1,7 +0,0 @@
-// This file is used to cache all dependencies
-import 'react';
-import 'react-dom';
-import '@astrojs/react';
-import 'astro';
-import '@deno/astro-adapter';
-console.log("All dependencies imported successfully!");
